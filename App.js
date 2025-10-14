a2442f9dfeadc7ba20104a5c28d86c5372fe411c
Enrique <86427434+EnriqueDeveloper@users.noreply.github.com>
2025-10-14 11:52:26 -0500

    chore: se instalo la libreria de datetimepicker y se creo variable y se modifico la seccion license

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function App() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+  const [daysLeft, setDaysLeft] = useState(null);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
